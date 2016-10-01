c092eda45027606cde1fc273dab22c90bb7d543b
Ivana Lieska <idrabikova@gmail.com>
2016-10-01 19:48:37 +0200

    Affine cipher :key:

---
+++ AffineCipher/AffineCipher/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AffineCipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AffineAlgorithm al = new AffineAlgorithm(new Message("AHOJ IVKA A TONOO"), new Key(110));
+            al.Encrypt();
+            al.Decrypt();
+        }
+    }
+}
---
+++ AffineCipher/AffineCipher/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffineCipher
+{
+    public class Message
+    {
+        public string message { get; private set; }
+        public Message(string message)
+        {
+            this.message = message;
+        }
+    }
+}
